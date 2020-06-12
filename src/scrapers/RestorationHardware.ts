de940d531ac5fb99bca535837b30523761395b20
Emma Munley <emmamunley@gmail.com>
2020-06-11 20:00:09 -0400

    RH working

---
+++ src/scrapers/RestorationHardware.ts
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { Scraper } from "./Scraper";
+import { RegistryItem } from "../types/registry/Registry";
+import { injectable } from "inversify";
+import puppeteer from "puppeteer";
+import cheerio from "cheerio";
+import { RegistrySource } from "../types/registry/RegistryTypes";
+import { LoggerFactory } from "../logger/LoggerFactory";
+import { formatUrl, formatPrice, formatQty } from "../utils/parsing";
+
+@injectable()
+export class RestorationHardware implements Scraper {
+    private static IGNORABLE_RESOURCE_TYPES = ["font"];
+    private static REGISTRY_SELECTOR = "table";
+    private static ITEM_SELECTOR = "tr.order-item-row";
+    private static REGISTRY_LOAD_TIMEOUT_MS = 5000;
+    private logger = LoggerFactory.getLogger(module);
+
+    public async scrape(url: string): Promise<RegistryItem[]> {
+        try {
+            const html = await this.getRestorationHardwareRegistryHTML(url);
+            const products = this.parseRestorationHardwareRegistryHTML(html);
+            return products;
+        } catch (error) {
+            this.logger.error(`error fetching html`, { error });
+            // todo: return an either type with an error code
+            return [];
+        }
+    }
+
+    private async getRestorationHardwareRegistryHTML(url: string): Promise<string> {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+
+        await page.setRequestInterception(true);
+        // Improve page load times by skipping useless requests
+        page.on("request", request => {
+            if (RestorationHardware.IGNORABLE_RESOURCE_TYPES.includes(request.resourceType())) {
+                request.abort();
+            } else {
+                request.continue();
+            }
+        });
+        await page.goto(url);
+        const registryHtml = await page.$eval(RestorationHardware.REGISTRY_SELECTOR, e => e.outerHTML);
+        await page.close();
+        await browser.close();
+        return registryHtml.toString();
+    }
+
+    private parseRestorationHardwareRegistryHTML(registryHtml: string): RegistryItem[] {
+        const $ = cheerio.load(registryHtml);
+        const items: RegistryItem[] = [];
+
+        $(RestorationHardware.ITEM_SELECTOR).each((_, productHTML) => {
+            const item = this.formatItem(productHTML);
+            items.push(item);
+        });
+
+        return items;
+    }
+
+    private formatItem(product: CheerioElement): RegistryItem {
+        const $ = cheerio.load(product);
+        const title = $(".order-item-name a")
+            .text()
+            .trim() as string;
+        let url = $("a").attr("href") as string;
+        url = formatUrl("www.rhmodern.com", url);
+        const priceText = $(".item-price__amount")
+            .text()
+            .trim();
+        const price = formatPrice(priceText);
+        let img = $(".order-item-image a img").attr("src") as string;
+        img = img.replace("//", "");
+        const requestedText = $(".gift-list-item-requested").text();
+        const requested = formatQty(requestedText);
+        const neededText = $(".gift-list-item-needs").text();
+        const needed = formatQty(neededText);
+        const purchased = requested - needed;
+
+        return {
+            title,
+            img,
+            price,
+            needed,
+            purchased,
+            url,
+            source: RegistrySource.RestorationHardware,
+        };
+    }
+}
