50fdf07a95cb638c5fc873c693ee468459b0f92b
Emma Munley <emmamunley@gmail.com>
2020-06-19 20:50:48 -0400

    views

---
+++ src/sql/registry_item/CreateRegistryItemView.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE VIEW registry_item_detailed AS
+SELECT
+    u.username,
+    r.title,
+    r.price,
+    r.needed,
+    r.purchased,
+    r.img,
+    r.url,
+    r.source
+FROM
+    registry_item AS r,
+    users AS u
+WHERE
+    u.id = r.user_id
